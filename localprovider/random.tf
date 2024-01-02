0ccc4e9ee9b75a0e1c52c8e053b5f93b2559a1e2
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-01-02 17:11:26 -0300

    Add provider, resource, and output variable declarations

--- localprovider/random.tf
+++ localprovider/random.tf
@@ -1,3 +1,4 @@
+# declare provider
 terraform {
     required_providers {
         random = {
@@ -7,12 +8,14 @@ terraform {
     }
 }
 
+# declare resource
 resource "random_string" "random" {
     length = 16
     special = false
     upper = false
 }
 
+# declare output variable
 output "random" {
     value = random_string.random.result
 }
