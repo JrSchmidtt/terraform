303ce078991c09ed11f90316bab7ad4ddfc3e06c
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-01-02 17:24:55 -0300

    Add archive and random providers and resources

---
+++ localprovider/archive/archive.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        archive = {
+            source = "hashicorp/archive"
+            version = "2.4.1"
+        }
+    }
+}
+
+# declare resource with name "zipfile" that will create a zip file from the contents of the data directory.
+resource "archive_file" "zipfile" { 
+    type = "zip"
+    source_dir = "data"
+    output_path = "data/backup.zip"
+}
