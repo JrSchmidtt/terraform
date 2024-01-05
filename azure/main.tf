5336b7c1af5b2dee6d81703d59f82a246d4144a1
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-01-05 20:38:14 -0300

    Add vnet-ip variable and update address_space in azurerm_virtual_network

--- azure/main.tf
+++ azure/main.tf
@@ -12,3 +12,18 @@ resource "azurerm_resource_group" "rg" {
     technology  = title("terraform") # title() function is used to convert the string to titlecase > Terraform.
   }
 }
+
+variable "vnet-ip" {
+  type = list(string)
+  default = ["10.0.0.0/16"]
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = concat(var.vnet-ip, ["192.168.0.0/16"])
+}
+
+output "vnet" {
+  value =  length(azurerm_virtual_network.vnet.address_space)
+}
