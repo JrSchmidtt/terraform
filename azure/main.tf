c1457fd6d9a95f248457d210265151810a89aae5
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-01-05 20:27:13 -0300

    Update resource group tags in main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -7,6 +7,7 @@ resource "azurerm_resource_group" "rg" {
   location = "brazilsouth"
   tags = {
     environment = lower("Dev") # lower() function is used to convert the string to lowercase > dev.
+    maintainer  = upper("Junior Schmidt") # upper() function is used to convert the string to uppercase > JUNIOR SCHMIDT.
     date        = formatdate("DD/MM/YYYY", timestamp()) # formatdate() function is used to format the date.
     technology  = title("terraform") # title() function is used to convert the string to titlecase > Terraform.
   }
