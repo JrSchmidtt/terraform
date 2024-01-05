6f989fc124d3f536de5690fd67677f243a0235a5
JrSchmidtt <junior.schmidt.99@gmail.com>
2024-01-05 20:25:29 -0300

    Update resource group tags in main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -6,7 +6,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "terraform-rg"
   location = "brazilsouth"
   tags = {
-    environment = "dev"
-    date        = formatdate("DD/MM/YYYY", timestamp())
+    environment = lower("Dev") # lower() function is used to convert the string to lowercase > dev.
+    date        = formatdate("DD/MM/YYYY", timestamp()) # formatdate() function is used to format the date.
+    technology  = title("terraform") # title() function is used to convert the string to titlecase > Terraform.
   }
 }
